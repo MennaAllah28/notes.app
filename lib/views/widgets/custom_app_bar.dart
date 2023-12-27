fafbcddb12c0637946d466b9b7e13ad31cbd7ba3
MennaAllah28 <mennaibrahim1m1@gmail.com>
2023-12-27 21:19:06 +0200

    custom app bar and search icon

---
+++ lib/views/widgets/custom_app_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Notes',
+          style: TextStyle(fontSize: 28),
+        )
+      ],
+    );
+  }
+}
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      width: 45,
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+          child: Icon(
+        Icons.search,
+        size: 28,
+      )),
+    );
+  }
+}
