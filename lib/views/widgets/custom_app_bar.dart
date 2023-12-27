375b7e632bb1f8a2093c5e76953c35bad51904dc
MennaAllah28 <mennaibrahim1m1@gmail.com>
2023-12-27 23:20:53 +0200

    custom note item

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -10,29 +11,10 @@ class CustomAppBar extends StatelessWidget {
         Text(
           'Notes',
           style: TextStyle(fontSize: 28),
-        )
+        ),
+        Spacer(),
+        CustomSearchIcon(),
       ],
-    );
-  }
-}
-
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 45,
-      width: 45,
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.05),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Center(
-          child: Icon(
-        Icons.search,
-        size: 28,
-      )),
     );
   }
 }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'custom_app_bar.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -15,6 +14,57 @@ class NotesViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NotesItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 16, bottom: 24, top: 24),
+      decoration: BoxDecoration(
+        color: Color(0xffFFCCB0),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 16),
+              child: Text(
+                'Build your career with Menna',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.5), fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.delete,
+                size: 24,
+                color: Colors.black,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Text(
+              'June22 , 2022',
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.4), fontSize: 16),
+            ),
+          )
         ],
       ),
     );
