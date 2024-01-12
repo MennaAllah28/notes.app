8657aa89b0cba50b7253da27b4867e83d838f43c
MennaAllah28 <mennaibrahim1m1@gmail.com>
2024-01-12 20:41:16 +0200

    display notes part one

--- lib/views/widgets/notes_list_vew.dart
+++ lib/views/widgets/notes_list_vew.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/notes_cubit/notes_cubit.dart';
+import '../../models/note_model.dart';
 import 'custom_item_note.dart';
 
 class NotesListView extends StatelessWidget {
@@ -6,16 +9,22 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(
-          padding: EdgeInsets.zero,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(vertical: 4),
-              child: NotesItem(),
-            );
-          }),
+    return BlocBuilder<NotesCubit, NotesState>(
+      builder: (context, state) {
+        List<NoteModel> notes = BlocProvider.of<NotesCubit>(context).notes!;
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          child: ListView.builder(
+              itemCount: notes.length,
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: NotesItem(),
+                );
+              }),
+        );
+      },
     );
   }
 }
